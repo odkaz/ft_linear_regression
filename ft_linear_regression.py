20f5e88d322a00de2e4c7657f01d7b505eb19675
odkaz <one.destiny.japan@gmail.com>
2023-03-08 18:34:08 +0100

    update:data normalisation

--- ft_linear_regression.py
+++ ft_linear_regression.py
@@ -20,6 +20,15 @@ def get_weight(bias, weight, milage, price):
         sum += (estimate_price(bias, weight, milage[i]) - price[i]) * milage[i]
     return sum / m
 
+def data_normalisation(data):
+    low = min(data)
+    high = max(data)
+    res = []
+    for x in data:
+        norm = (x - low) / (high - low)
+        res.append(norm)
+    return res, low
+
 def linear_regression():
     df = pd.read_csv('./data.csv')
     km = df["km"].to_list()
@@ -27,16 +36,16 @@ def linear_regression():
     x_train = np.array(km)
     y_train = np.array(price)
     learning_rate = 0.001
-    iterate = 10
-    theta0, theta1 = 00,0
+    iterate = 100000
+    theta0, theta1 = 0,0
+    x_norm, norm = data_normalisation(x_train)
 
     for _ in range(0, iterate):
         tmp0, tmp1 = theta0, theta1
-        theta0 -= learning_rate * get_bias(tmp0, tmp1, x_train, y_train)
-        theta1 -= learning_rate * get_weight(tmp0, tmp1, x_train, y_train)
-        print('theta0', theta0, 'theta1', theta1)
+        theta0 -= learning_rate * get_bias(tmp0, tmp1, x_norm, y_train)
+        theta1 -= learning_rate * get_weight(tmp0, tmp1, x_norm, y_train)
 
-    res_y = [estimate_price(theta0, theta1, x) for x in x_train]
+    res_y = [estimate_price(theta0, theta1, x) for x in x_norm]
     plt.plot(x_train, res_y, c='b', label='Our Prediction')
     plt.scatter(x_train, y_train, marker='x', c='r', label='Actual Values')
     plt.show()
